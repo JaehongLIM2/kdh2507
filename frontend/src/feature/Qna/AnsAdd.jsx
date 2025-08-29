13efd211d404f3df2fd8c74f691e00083204fa5d
JaehongLIM2 <nu0044@naver.com>
2025-08-29 15:07:41 +0900

    お問い合わせ回答ページUIを日本語に変更

--- frontend/src/feature/Qna/AnsAdd.jsx
+++ frontend/src/feature/Qna/AnsAdd.jsx
@@ -1,15 +1,5 @@
-import {
-  Button,
-  Col,
-  FormControl,
-  FormGroup,
-  FormLabel,
-  Image,
-  Modal,
-  Row,
-  Spinner,
-} from "react-bootstrap";
-import { useContext, useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 import { useNavigate, useSearchParams } from "react-router";
@@ -24,12 +14,19 @@ export function AnsAdd() {
   const [isProcessing, setIsProcessing] = useState(false);
   const navigate = useNavigate();
   const categoryList = {
-    1: { value: "기능 관련" },
-    2: { value: "크기·무게 관련" },
-    3: { value: "배송 관련" },
-    4: { value: "설정 관련" },
-    5: { value: "반품·교환 관련" },
-    6: { value: "기타 문의" },
+    1: { value: "注文／決済" },
+    2: { value: "配送関連" },
+    3: { value: "キャンセル／返金" },
+    4: { value: "返品／交換" },
+    5: { value: "領収書・請求書発行" },
+    6: { value: "ログイン／会員情報" },
+    7: { value: "サービス／その他" },
+    // 1: { value: "기능 관련" },
+    // 2: { value: "크기·무게 관련" },
+    // 3: { value: "배송 관련" },
+    // 4: { value: "설정 관련" },
+    // 5: { value: "반품·교환 관련" },
+    // 6: { value: "기타 문의" },
   };
 
   useEffect(() => {
@@ -41,9 +38,7 @@ export function AnsAdd() {
       .catch((err) => {
         console.log(err);
       })
-      .finally(() => {
-        console.log("always");
-      });
+      .finally(() => {});
   }, []);
 
   if (!question) {
@@ -59,7 +54,6 @@ export function AnsAdd() {
         answer: answer,
       })
       .then((res) => {
-        console.log("성공");
         const message = res.data.message;
         if (message) {
           toast(message.text, { type: message.type });
@@ -67,7 +61,6 @@ export function AnsAdd() {
         navigate("/qna/list");
       })
       .catch((err) => {
-        console.log("오류");
         const message = err.response.data.message;
         if (message) {
           // toast 띄우기
@@ -75,7 +68,6 @@ export function AnsAdd() {
         }
       })
       .finally(() => {
-        console.log("always");
         setIsProcessing(false);
       });
   }
@@ -84,12 +76,14 @@ export function AnsAdd() {
     <Row className="justify-content-center">
       <Col md={8} lg={6} className="mt-5">
         <div className="container">
-          <h2 className="mb-4">문의 내역 상세</h2>
+          {/*문의 내역 상세*/}
+          <h2 className="mb-4">お問い合わせ詳細</h2>
           <div className="row">
             <div>
               <div className="form-control w-full mb-3">
                 <label className="label">
-                  <span className="label-text">상담유형</span>
+                  {/*상담 유형*/}
+                  <span className="label-text">お問い合わせ種別</span>
                 </label>
                 <input
                   type="text"
@@ -104,7 +98,8 @@ export function AnsAdd() {
             <div>
               <div className="form-control w-full mb-4">
                 <label className="label">
-                  <span className="label-text">문의하실 상품</span>
+                  {/*문의하실 상품*/}
+                  <span className="label-text">お問い合わせ対象商品</span>
                 </label>
                 <div>
                   {/* 상품 이미지 */}
@@ -118,12 +113,12 @@ export function AnsAdd() {
                     type="text"
                     className="input input-bordered w-full mb-2"
                     value={question.product}
-                    placeholder="상품명"
+                    placeholder="商品名" // 상품명
                     disabled
                   />
                   {/* 가격 (오른쪽 정렬, 굵은 빨간색) */}
                   <h5 className="text-right font-bold text-red-600">
-                    {question.price.toLocaleString()}원
+                    {question.price.toLocaleString()} 円
                     {/* toLocaleString(): 세 자리마다 콤마 표시 */}
                   </h5>
                 </div>
@@ -134,7 +129,8 @@ export function AnsAdd() {
             <div>
               <div className="form-control w-full mb-4">
                 <label className="label">
-                  <span className="label-text">제목</span>
+                  {/*제목*/}
+                  <span className="label-text">件名</span>
                 </label>
                 <input
                   type="text"
@@ -150,7 +146,8 @@ export function AnsAdd() {
             <div>
               <div className="form-control w-full mb-3">
                 <label className="label">
-                  <span className="label-text">문의 내용</span>
+                  {/*문의 내용*/}
+                  <span className="label-text">お問い合わせ内容</span>
                 </label>
                 <textarea
                   className="textarea textarea-bordered w-full"
@@ -163,7 +160,8 @@ export function AnsAdd() {
             <div>
               <div className="form-control w-full mb-3">
                 <label className="label">
-                  <span className="label-text">답변 내용</span>
+                  {/*답변 내용*/}
+                  <span className="label-text">回答内容</span>
                 </label>
                 <textarea
                   className="textarea textarea-bordered w-full"
@@ -179,7 +177,8 @@ export function AnsAdd() {
                 className="btn btn-primary ml-2"
                 onClick={setModalShow}
               >
-                답변 등록
+                {/*답변 등록*/}
+                回答を登録する
               </button>
             </div>
           </div>
@@ -188,24 +187,29 @@ export function AnsAdd() {
       {modalShow && (
         <div className="modal modal-open">
           <div className="modal-box">
-            <h3 className="font-bold text-lg mb-3">답변 등록 확인</h3>
+            {/*답변 등록 확인*/}
+            <h3 className="font-bold text-lg mb-3">回答登録の確認</h3>
             <p className="mb-6">
-              답변을 등록하면 수정할 수 없습니다.
+              {/*답변을 등록하면 수정할 수 없습니다.*/}
+              回答を登録すると修正できません。
               <br />
-              등록하시겠습니까?
+              {/*등록하시겠습니까?*/}
+              登録してもよろしいですか？
             </p>
             <div className="modal-action">
               <button
                 className="btn btn-outline btn-neutral"
                 onClick={() => setModalShow(false)}
               >
-                취소
+                {/*취소*/}
+                戻る
               </button>
               <button
                 className="btn btn-primary"
                 onClick={handleAnswerButtonClick}
               >
-                답변완료
+                {/*답변완료*/}
+                回答完了
               </button>
             </div>
             <button
